3d61be4d38c4af521b0e6732fcebb4447b92ce6b
Ian Karlsson <ian.karlsson@niradynamics.se>
2019-10-23 10:49:10 +0200

    niracar
    Change-Id: I232e878bf41bf889b4c7946760bb16657956acd7

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import sys, pygame
+import NiraCar
 pygame.init()
 
 if __name__ == '__main__':
@@ -7,10 +8,17 @@ if __name__ == '__main__':
     black = 0,0,0
     screen = pygame.display.set_mode(screen_size)
     screen.fill(black)
+    nira_car = NiraCar.NiraCar(500,500,1024,864)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+
+        screen.fill((0,0,0))
+
+        nira_car.UpdatePlayer()
+        nira_car.UpdateObject()
+        nira_car.Draw(screen)
 
         # displays what we drawn.
         pygame.display.flip()
