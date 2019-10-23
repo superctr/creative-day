1f42d2f653786b9556718a8cdd677d033d83687f
Alex Jukimenko <alex.jukimenko@niradynamics.se>
2019-10-23 13:08:24 +0200

    Firts obstacle added NOK
    Change-Id: I465773aafb31ee74f198382ebf07f7047acf5cdc

---
+++ PlayerCar.py
@@ -0,0 +1,18 @@
+import sys, pygame
+
+class Pleer:
+    def __init__(self,x,y,screen_x,screen_y):
+        self.object = pygame.image.load("Sprites/Veoneer_Car.png")
+        self.x = x
+        self.y = y
+        self.screen_x = screen_x
+        self.screen_y = screen_y
+
+    def UpdatePlayer(self):
+        pass
+
+    def UpdateObject(self):
+        pass
+
+    def Draw(self, screen):
+        screen.blit(self.object, (self.x, self.y))
