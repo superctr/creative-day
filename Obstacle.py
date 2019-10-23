e5b47ae12f169bc81f6c14585e38965e96a922ad
Alex Jukimenko <alex.jukimenko@niradynamics.se>
2019-10-23 13:42:01 +0200

    obticle OK
    Change-Id: I5bb658c747e16ee72b152caa7b4b4e3270470411

--- Obstacle.py
+++ Obstacle.py
@@ -3,27 +3,30 @@ import numpy as np
 import math
 import random
 
-
-class velocity():
-    def __init__(self, x, y):
-        speed, direction = self.normalizeVector(x, y)
-        self.speed = speed
-        self.direction = direction
-
-    def normalizeVector(self, x, y):
-        speed = math.sqrt(math.pow(x,2) + math.pow(y,2))
-        newV = [x/speed, y/speed]
-        return speed, newV
-
 class obsticleClass():
-    def __init__(self):
+    def __init__(self,screen_width, screen_height):
         self.x, self.y = 100, 100
-        self.width = 50
+        self.width = 100
         self.height = 100
         self.color = (0,255,0)
-        self.vx, self.vy = 0, 0
-        #self.direction = Velocity()
-        self.object = pygame.image.load("Sprites/obsticle.png")
+
+        self.vx, self.vy = random.randint(-10,10), random.randint(-10,10)
+        if abs(self.vx)>abs(self.vy):
+            self.y = random.randint(0,screen_width)
+            if self.vx>0:
+                self.x = 0
+            else:
+                self.x = screen_width
+        else:
+            self.x = random.randint(0,screen_height)
+            if self.vy>0:
+                self.y = 0
+            else:
+                self.y = screen_height
+
+
+        self.object = pygame.image.load("Sprites/obsticle3.png")
+        self.object = pygame.transform.scale(self.object,(self.width,self.height))
 
     def update(self, keys, screen_width, screen_height):
         self.x += self.vx
