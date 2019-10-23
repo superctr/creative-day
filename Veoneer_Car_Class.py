5f3d9dc6cd00089ccf2f562430070c3c862bd232
Adrian Corral <adcz@syntronic.com>
2019-10-23 16:10:35 +0200

    Change shooting from Ctrol to B

--- Veoneer_Car_Class.py
+++ Veoneer_Car_Class.py
@@ -31,7 +31,7 @@ class drawVeoneerClass():
 
         if self.bullet_timeout > 0:
             self.bullet_timeout = self.bullet_timeout - 1
-        elif keys[pygame.K_LCTRL]:
+        elif keys[pygame.K_b]:
             # Space is held, there is an autofire limit
             obstacle_list.append(bullet(screen_width,screen_height,self.center_x,self.center_y,self.angle,1))
             self.bullet_timeout = 9
