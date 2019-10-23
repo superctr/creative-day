725376dc4cd5a0f566380bbe832ba4e56d7d1d97
Ian Karlsson <ian.karlsson@niradynamics.se>
2019-10-23 13:09:46 +0200

    Add car sprite

--- how_to_class.py
+++ how_to_class.py
@@ -2,6 +2,7 @@ import sys, pygame, math
 
 class drawClass():
     def __init__(self):
+        self.object = pygame.image.load("Sprites/Nira_Car.jpg")
         self.x, self.y = 100, 100
         self.width = 50
         self.height = 100
@@ -26,5 +27,12 @@ class drawClass():
         self.y += math.sin(radians) * self.speed
 
     def draw_rectangle(self,screen):
-        rectangle = (self.x, self.y, self.width, self.height)
-        pygame.draw.rect(screen, self.color, rectangle)
+
+        #rectangle = (self.x, self.y, self.width, self.height)
+        #pygame.draw.rect(screen, self.color, rectangle)
+        center = self.object.get_rect().center
+        rotated_image = pygame.transform.rotate(self.object, -self.angle)
+        new_rect = rotated_image.get_rect(center = center)
+        new_rect.x += self.x
+        new_rect.y += self.y
+        screen.blit(rotated_image, new_rect.topleft)
