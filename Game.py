53aae6bcad4d56d61f83a58285fce698ea2f3f95
Gunnar Winter <gunnar.winter@niradynamics.se>
2019-10-23 10:17:55 +0200

    Minor fix...
    Change-Id: Ie2c41041a67e582f1f41c3f3e6a82c3d07d701fe

--- Game.py
+++ Game.py
@@ -1,12 +1,7 @@
 import sys, pygame
-pygame.init()
-
-if __name__ == '__main__':
-
-    screen_size = screen_width, screen_height = 1024, 864
-    black = 0,0,0
-    screen = pygame.display.set_mode(screen_size)
-    screen.fill(black)
+import threading, time
+from gameModules import InputHandler
+def Main():
 
     while True:
         for event in pygame.event.get():
@@ -14,3 +9,12 @@ if __name__ == '__main__':
 
         # displays what we drawn.
         pygame.display.flip()
+    pygame.quit()
+
+if __name__ == '__main__':
+
+    screen_size = screen_width, screen_height = 1024, 864
+    black = 0,0,0
+    screen = pygame.display.set_mode(screen_size)
+    pygame.init()
+    Main()
