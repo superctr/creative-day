e803f5e18fec14b65ece5b99c9ca1540945cbc1a
Adrian Corral <adcz@syntronic.com>
2019-10-23 09:51:36 +0200

    Move initialization to main function

--- Game.py
+++ Game.py
@@ -1,16 +1,16 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 1024, 768
-black = 0,0,0
-screen = pygame.display.set_mode(size)
+if __name__ == '__main__':
 
-
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
+    screen_size = screen_width, screen_height = 1024, 864
+    black = 0,0,0
+    screen = pygame.display.set_mode(screen_size)
     screen.fill(black)
 
-    # displays what we drawn.
-    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        # displays what we drawn.
+        pygame.display.flip()
